7fdb05c652346f9a9b1fa7c9ebfb529e63745ba7
Fondit <bastien.verdiervaissiere@gmail.com>
2023-01-08 05:11:18 +0100

    organisation fichier

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
         <meta name='viewport' content='width=device-width, initial-scale=1.0'>
         <title>LN institut</title>
 
-        <link rel='stylesheet' href='./styles/default.css'>
-        <link rel='stylesheet' href='./styles/index.css'>
-        <link rel='stylesheet' href='./styles/responsive.css'>
+        <link rel='stylesheet' href='./src/styles/default.css'>
+        <link rel='stylesheet' href='./src/styles/index.css'>
+        <link rel='stylesheet' href='./src/styles/responsive.css'>
     </head>
     <body>
         <header>
@@ -20,12 +20,12 @@
             <div id="rightHeader"></div>
         </header>
         <main>
-            
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2885.310850001462!2d1.4311845154117255!3d43.68330055850189!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12aea46d84fe7c3f%3A0x4f2da1bb6eda942!2s10%20Rue%20Didier%20Daurat%2C%2031140%20Fonbeauzard!5e0!3m2!1sfr!2sfr!4v1673149253875!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </main>
         <footer>
             
         </footer>
-        <script src='./js/data.js'></script>
-        <script src='./js/main.js'></script>
+        <script src='./src/js/data.js'></script>
+        <script src='./src/js/main.js'></script>
     </body>
 <html>
